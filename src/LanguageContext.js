4ae3269a0fb3aff853842deef029a3fb83b670e5
MandyNeumeyer <64845397+MandyNeumeyer@users.noreply.github.com>
2022-01-11 17:08:59 +0100

    Update LanguageContext.js

--- src/LanguageContext.js
+++ src/LanguageContext.js
@@ -4,5 +4,5 @@ import { createContext } from 'react';
 // Wir initialisieren unseren context, und stellen den initial/oder defaultwert auf "null".
 const LanguageContext = createContext();
 
-// Wir exportieren unseren neuen context in App.js um ihn dort um als Elternteil um alle Kinder der App.js zu legen.
+// Wir exportieren unseren neuen context in App.js um ihn dort als Elternteil um alle Kinder der App.js zu legen.
 export default LanguageContext 
