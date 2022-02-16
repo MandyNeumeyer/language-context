1b4c6e73d8964bdfba70de1e745aa5d513fdc590
mandy <mandy.neumeyer@web.de>
2022-02-16 16:09:33 +0100

    init

--- src/LanguageContext.js
+++ src/LanguageContext.js
@@ -1,7 +1,7 @@
 // We import createContext from react.
 import { createContext } from 'react';
 
-// We initialise our context, and set the initial/ default value to "null".
+// We initialise our context, and set the initial-/ default value to "null".
 const LanguageContext = createContext();
 
 // We export our new context.
